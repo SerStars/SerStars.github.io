02c0cd9c178cb4e33e4619d7a5d7365a3feab114
SerStars <90714930+SerStars@users.noreply.github.com>
2024-03-10 15:54:39 +0100

    New website layout

---
+++ index.js
@@ -0,0 +1,24 @@
+(() => {
+
+    const time = document.getElementById("tijd")
+    
+    const timeFunc = () => {
+        const currentTime = new Date().toLocaleString("en-US", { timeZone: "Europe/Amsterdam" }).split(", ")[1].split(":");
+        const hours = parseInt(currentTime[0]);
+
+        time.innerHTML = `${hours}:${currentTime[1]}:${currentTime[2]}`;
+    }
+
+
+    setTimeout(() => {
+        setInterval(timeFunc, 1000);
+    }, 500);
+})()
+
+var myImageElement = document.getElementById('discordstatus');
+myImageElement.src = 'https://lanyard.cnrad.dev/api/861631850681729045?theme=dark&bg=transparant&idleMessage=Doing+Nothing' + Math.random();
+
+setInterval(function() {
+    var myImageElement = document.getElementById('discordstatus');
+    myImageElement.src = 'https://lanyard.cnrad.dev/api/861631850681729045?theme=dark&bg=transparant&idleMessage=Doing+Nothing' + Math.random();
+}, 500);
