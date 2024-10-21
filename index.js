76989cec71ac9642d873f6b4dd5fc911dd12df01
wont.stream <drns.pw@icloud.com>
2024-10-21 17:45:30 -0400

    linting and formatting

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-(() => {
+import "https://cdn.jsdelivr.net/npm/beercss@3.7.10/dist/cdn/beer.min.js";
 
-    const time = document.getElementById("tijd")
+const discord = document.getElementById("discord");
 
-    const timeFunc = () => {
-        const currentTime = new Date().toLocaleString("en-US", { timeZone: "Europe/Amsterdam" }).split(", ")[1].split(":");
-        const hours = parseInt(currentTime[0]);
+const updateDiscord = async () => {
+	const res = await fetch(
+		"https://corsproxy.io/?https%3A%2F%2Flanyard.cnrad.dev%2Fapi%2F861631850681729045%3Ftheme%3Ddark%26bg%3Dtransparant%26idleMessage%3D%7BTIME_REPLACE%7D",
+	);
+	const data = await res.text();
+	discord.innerHTML = data.replace("{TIME_REPLACE}", `<span id="time"></span>`);
+};
 
-        time.innerHTML = `${hours}:${currentTime[1]}:${currentTime[2]}`;
-    }
+const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
 
+lanyard.onmessage = ({ data }) => {
+	const { op, d } = JSON.parse(data);
+	switch (op) {
+		case 0: {
+			updateDiscord();
 
-    setTimeout(() => {
-        setInterval(timeFunc, 1000);
-    }, 500);
-})()
-const time = document.getElementById("tijd")
+			break;
+		}
+		case 1: {
+			// Setup Heartbeat
+			setInterval(() => {
+				lanyard.send(JSON.stringify({ op: 3 }));
+			}, d.heartbeat_interval);
+			// Subscribe to user id 1273447359417942128
+			lanyard.send(
+				JSON.stringify({
+					op: 2,
+					d: {
+						subscribe_to_id: "861631850681729045",
+					},
+				}),
+			);
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+};
 
 setInterval(() => {
-    const currentTime = new Date().toLocaleString("en-US", { timeZone: "Europe/Amsterdam" }).split(", ")[1].split(":");
-    const hours = parseInt(currentTime[0]);
+	const currentTime = new Date()
+		.toLocaleString("en-US", { timeZone: "Europe/Amsterdam" })
+		.split(", ")[1]
+		.split(":");
+	const hours = Number.parseInt(currentTime[0]);
 
-    time.innerHTML = `${hours}:${currentTime[1]}:${currentTime[2]}`;
-})
+	const time = document.getElementById("time");
+
+	if (time) {
+		time.innerHTML = `${hours}:${currentTime[1]}:${currentTime[2]}`;
+	}
+});
